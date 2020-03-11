841a8f4cb5dbf3052d22901c68280a7f940eb695
devWarior <kilshiki@rambler.ru>
2020-03-11 19:30:56 +0500

    Add function 'getUsers' and callback in the button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,16 +5,17 @@ import userPhoto from "../../assets/images/userPhoto.png"
 
 
 const Users = (props) => {
-
+let getUsers = () => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
 
             props.setUsers(response.data.items)
         })
     }
-
+}
 
     return <div>
+        <button onClick={getUsers}>Показать пользователей</button>
         {props.users.map(u =>
             <div key={u.id}>}
                 <img className={style.userAvatar} src={u.photos.small != null ? u.photos.small: userPhoto} alt={""}/>
