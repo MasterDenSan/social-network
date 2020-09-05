c1776edabf3baf92098e0e1dad8469881f683fe8
devWarior <kilshiki@rambler.ru>
2020-09-05 14:51:51 +0500

    Fixed Bags

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -38,13 +38,6 @@ const PostForm = (props) => {
             <div>
                 <button>Add Post</button>
             </div>
-            <div>
-<<<<<<< HEAD
-               {/* <button>Remove</button>*/}
-=======
-                {/*<button>Remove</button>*/}
->>>>>>> 11ffb00bac714e46502ec2377d1dd5e237c7114b
-            </div>
         </form>
     )
 }
