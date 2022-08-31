1db19dcde396faadd6c3c67b66b704416b71cebe
denislamH <kilshiki@rambler.ru>
2022-08-31 22:08:31 +0500

    typed withauthredirect

--- src/HOC/withAuthRedirect.tsx
+++ src/HOC/withAuthRedirect.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {FC} from "react";
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
+import {RootState} from "../redux/redux-store";
 
 
-let mapStateToPropsContainer = (state) => ({
+export type AuthRedirectT = {
+    isAuth: boolean
+}
+
+let mapStateToPropsContainer = (state:RootState) => ({
     isAuth: state.auth.isAuth
 })
 
 
-export const withAuthRedirect = (Component) => {
+export const withAuthRedirect = (Component: FC<AuthRedirectT>) => {
 
-    class AuthRedirect extends React.Component {
+    class AuthRedirect extends React.Component<AuthRedirectT> {
 
         render() {
             if (!this.props.isAuth) return <Redirect to={"/login"}/>
