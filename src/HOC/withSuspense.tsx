2fe7144ee90ce380fb7c1a5c6024442469dab762
denislamH <kilshiki@rambler.ru>
2022-09-02 17:44:55 +0500

    typed wwithSuspense and FormControls components

--- src/HOC/withSuspense.tsx
+++ src/HOC/withSuspense.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {FC} from "react";
 
 
-export const WithSuspense = (Component) => {
-    return (props) => {
+export const WithSuspense = (Component: FC) => {
+    return (props: any) => {
         return <React.Suspense fallback={<div>Loading...</div>}>
             <Component {...props}/>
         </React.Suspense>
